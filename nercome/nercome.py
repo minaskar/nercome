777988e62b93b87967f02db50a39e5227fc08d53
minaskar <minaskar@gmail.com>
2020-11-24 16:37:46 +0000

    cov.T

--- nercome/nercome.py
+++ nercome/nercome.py
@@ -1,7 +1,10 @@
 import numpy as np
 
 
-def cov(m, fs=2.0/3.0, n=50):
+def cov(m, fs=2.0/3.0, n=100, rowvar=True):
+
+    if rowvar:
+        m = m.T
 
     nsamples, ndim = m.shape
 
@@ -13,6 +16,7 @@ def cov(m, fs=2.0/3.0, n=50):
     cov = np.zeros((ndim, ndim))
 
     for i in range(n):
+
         choice = np.random.choice(idx, size=s, replace=False)
         mask1 = np.in1d(idx, choice)
         mask2 = ~mask1
@@ -23,7 +27,7 @@ def cov(m, fs=2.0/3.0, n=50):
         cov2 = np.cov(x2.T)
 
         _, eigvec = np.linalg.eigh(cov1)
-        mid_matrix_diag = np.diag(np.diag(eigvec.T @ cov2 @ eigvec))
-        cov += eigvec @ mid_matrix_diag @ eigvec.T
+        D = np.diag(np.diag(eigvec.T @ cov2 @ eigvec))
+        cov += eigvec @ D @ eigvec.T
 
     return cov / n
